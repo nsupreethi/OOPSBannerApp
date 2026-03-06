363d6b14cfad37ae01aeb72d0de0cff09130a042
nsupreethi <nsupreethi007@gmail.com>
2026-03-06 18:45:11 +0530

    Initial commit

---
+++ OOPSBannerApp.java
@@ -0,0 +1,15 @@
+/**
+ * OOPSBannerApp
+ * A simple Java program to print OOPS to the console.
+ * 
+ * @author Supreethi
+ * @version 1.0
+ */
+
+public class OOPSBannerApp {
+
+    public static void main(String[] args) {
+        System.out.println("OOPS");
+    }
+
+}
